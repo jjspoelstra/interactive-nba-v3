fdb7e1135f45d2d7cbfc53aba2ab229b05c3e535
Joe Spoelstra <jjspoels@gmail.com>
2023-07-21 17:37:11 -0400

    fixed useclient error

--- firebase/AuthContext.jsx
+++ firebase/AuthContext.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
 import {app} from './firebaseConfig'
